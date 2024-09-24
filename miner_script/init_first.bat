af7635e092578a192b1b8252662a7c7f6d040e8e
davidleeeeee <exmple@email.com>
2024-09-24 10:01:45 +0800

    timestamp

--- miner_script/init_first.bat
+++ miner_script/init_first.bat
@@ -19,18 +19,18 @@ echo Miner Address: %MINER_ADDRESS%
 "%~dp0geth.exe" --datadir "%~dp0data" init "%~dp0zkscam.json"
 
 :: Start Geth node with all configurations passed via command line, enabling HTTP API
-start "" cmd /c "%~dp0geth.exe --datadir "%~dp0data" --port 30303 --ipcpath "%~dp0geth.ipc"   --http --http.addr 0.0.0.0 --http.port 8545 --allow-insecure-unlock --http.api personal,eth,net,web3,txpool,miner,admin --http.corsdomain '*' --http.vhosts localhost,127.0.0.1 --networkid 63658 --bootnodes enode://53d15844753546eb04764d2340f6a118d5c8c58b47c2362f023688c210ebe24f0533132c61a32d064538c7c71bd2689040d5c8f41c34a5eaa3ac348a2d77fbfa@103.97.58.18:30303 --miner.etherbase %MINER_ADDRESS%  console"
+start "" cmd /c "%~dp0geth.exe --datadir "%~dp0data" --port 30303 --ipcpath "%~dp0geth.ipc"   --http --http.addr 0.0.0.0 --http.port 8545 --allow-insecure-unlock --http.api personal,eth,net,web3,txpool,miner,admin --http.corsdomain '*' --http.vhosts localhost,127.0.0.1 --networkid 63658 --bootnodes enode://719e72409b654c3134fb83a9ea62fb262cd1c8df30c2b1991b855a87d50fc9f1ff72df80fe837e37d4325c0f9fdce221068824bda46f9c7d1379b9104c761f9d@103.97.58.18:30303 --miner.etherbase %MINER_ADDRESS%  console"
 
 
 ::需要管理员权限运行才不会报错
 timeout /t 10
 :: Import private key into Geth account (显示导入结果)
 echo Importing private key...
-curl -X POST --data "{\"jsonrpc\":\"2.0\",\"method\":\"personal_importRawKey\",\"params\":[\"%PRIVATE_KEY%\", \"your-password\"],\"id\":1}" -H "Content-Type: application/json" http://localhost:8545> "%~dp0Importing_status.json" 2>>"%LOG_FILE%"
+curl -X POST --data "{\"jsonrpc\":\"2.0\",\"method\":\"personal_importRawKey\",\"params\":[\"%PRIVATE_KEY%\", \"123\"],\"id\":1}" -H "Content-Type: application/json" http://localhost:8545> "%~dp0Importing_status.json" 2>>"%LOG_FILE%"
 
 :: Unlock account (显示解锁结果)
 echo Unlocking account...
-curl --unix-socket "%~dp0geth.ipc" -X POST --data "{\"jsonrpc\":\"2.0\",\"method\":\"personal_unlockAccount\",\"params\":[\"%MINER_ADDRESS%\", \"your-password\", 0],\"id\":1}" -H "Content-Type: application/json" http://localhost> "%~dp0Unlocking_status.json" 2>>"%LOG_FILE%"
+curl --unix-socket "%~dp0geth.ipc" -X POST --data "{\"jsonrpc\":\"2.0\",\"method\":\"personal_unlockAccount\",\"params\":[\"%MINER_ADDRESS%\", \"123\", 0],\"id\":1}" -H "Content-Type: application/json" http://localhost> "%~dp0Unlocking_status.json" 2>>"%LOG_FILE%"
 
 
 
